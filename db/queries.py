63eb21128132730034b54dca1ff1dc91a273c206
KerQzi <jadikovkyrgyz@gmail.com>
2024-09-28 19:20:34 +0300

    TEST

---
+++ db/queries.py
@@ -0,0 +1,16 @@
+CREATE_TABLE_PRODUCTS = """
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name_product VARCHAR(255),
+        category_product VARCHAR(255),
+        size_product VARCHAR(255),
+        price_product VARCHAR(255),
+        product_id VARCHAR(255),
+        photo TEXT
+    )
+"""
+
+INSERT_PRODUCTS_QUERY = """
+    INSERT INTO products (name_product, category_product, size_product, price_product, product_id, photo)
+    VALUES (?, ?, ?, ?, ?, ?)
+"""
